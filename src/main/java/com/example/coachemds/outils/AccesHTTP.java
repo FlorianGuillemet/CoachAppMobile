baceaf5feec0757f95fe0ed18526b55db8dda7d7
Florian <f.guillemet@live.fr>
2019-03-27 11:50:04 +0100

    Modification du Graddle + ajout et code de la classe AccesHTTP

---
+++ src/main/java/com/example/coachemds/outils/AccesHTTP.java
@@ -0,0 +1,74 @@
+package com.example.coachemds.outils;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+
+public class AccesHTTP extends AsyncTask<String, Integer, Long>
+{
+
+    private ArrayList<NameValuePair> parametres;
+    private String retour = null;
+
+    /**
+     * Constructeur
+     */
+    public AccesHTTP()
+    {
+        parametres = new ArrayList<NameValuePair>();
+    }
+
+    /**
+     * ajout d'un parametre post
+     * @param nom
+     * @param valeur
+     */
+    public void addParam(String nom, String valeur)
+    {
+        parametres.add(new BasicNameValuePair(nom, valeur));
+    }
+
+    /**
+     * Connexion en tâche de fond dans un thread séparé
+     * @param strings
+     * @return
+     */
+    @Override
+    protected Long doInBackground(String... strings)
+    {
+        HttpClient cnxHttp = new DefaultHttpClient();
+        HttpPost paramCnx = new HttpPost(strings[0]);
+
+        try
+        {
+            // encodage des parametres
+            paramCnx.setEntity(new UrlEncodedFormEntity(parametres));
+            // connexion et envoi des parametres, attente de reponse
+            HttpResponse reponse = cnxHttp.execute(paramCnx);
+            // transformation de la reponse
+            retour = EntityUtils.toString(reponse.getEntity());
+
+        } catch (UnsupportedEncodingException e) {
+            Log.d("Erreur encodage", "**************"+e.toString());
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
