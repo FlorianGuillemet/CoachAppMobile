7511e3e104b247b5e87b743733cd1168b2b00bc6
Florian <f.guillemet@live.fr>
2019-03-28 13:35:18 +0100

    code de la methode de conversion de chaine en date + modif AccesLocal

--- src/main/java/com/example/coachemds/outils/MesOutils.java
+++ src/main/java/com/example/coachemds/outils/MesOutils.java
@@ -1,6 +1,35 @@
 package com.example.coachemds.outils;
 
+import android.util.Log;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public abstract class MesOutils
 {
+    /**
+     * Conversion chaine sous format Thu Mar 28 12:22:52 GMT 2019 (EEE MMM dd hh:mm:ss 'GMT' yyyy) vers date
+     * @param uneDate
+     * @return
+     */
+    public static Date convertStringToDate(String uneDate)
+    {
+        String formatAttendu = "EEE MMM dd hh:mm:ss 'GMT' yyyy";
 
+        SimpleDateFormat formatter = new SimpleDateFormat(formatAttendu);
+
+        try
+        {
+            Date date = formatter.parse(uneDate);
+
+            return date;
+
+        } catch (ParseException e) {
+            Log.d("Erreur", "******parse de la date impossible*******"+e.toString());
+            e.printStackTrace();
+        }
+
+        return null;
+    }
 }
