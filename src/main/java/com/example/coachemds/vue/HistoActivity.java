ff1a1418079507c81cfc0839431ca119f0a851cd
Florian <f.guillemet@live.fr>
2019-03-29 09:24:17 +0100

    Ajout de HistoActivity

---
+++ src/main/java/com/example/coachemds/vue/HistoActivity.java
@@ -0,0 +1,15 @@
+package com.example.coachemds.vue;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.coachemds.R;
+
+public class HistoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_histo);
+    }
+}
