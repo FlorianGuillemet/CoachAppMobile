2cc7b97ce8e7434c1174a9503145d0f25a9ab710
Florian <f.guillemet@live.fr>
2019-03-29 10:44:30 +0100

    code de la methode creerListe de HistoActivity

--- src/main/java/com/example/coachemds/vue/HistoActivity.java
+++ src/main/java/com/example/coachemds/vue/HistoActivity.java
@@ -5,16 +5,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ListView;
 
 import com.example.coachemds.R;
+import com.example.coachemds.controleur.Controle;
+import com.example.coachemds.model.Profil;
+
+import java.util.ArrayList;
 
 public class HistoActivity extends AppCompatActivity {
+
+    private Controle controle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_histo);
+        this.controle = Controle.getInstance(this);
         ecouteRetourMenu();
+        creerListe();
     }
 
     /**
@@ -33,4 +42,19 @@ public class HistoActivity extends AppCompatActivity {
             }
         });
     }
+
+    /**
+     * creer la liste adapter
+     */
+    private void creerListe()
+    {
+        ArrayList<Profil> lesProfils = controle.getLesProfils();
+
+        if(lesProfils != null) {
+            ListView lstHisto = (ListView)findViewById(R.id.lstHisto);
+            HistoListAdapter adapter = new HistoListAdapter(this, lesProfils);
+            lstHisto.setAdapter(adapter);
+        }
+    }
+
 }
